83fe486d4df9d57b7c21a2ac6e186b2dd0925d12
novice81 <novice81.github@gmail.com>
2020-02-25 00:00:39 +0200

    Integrate class library with CLI
    $ dotnet add SampleCalc reference SampleCalc.Core
    
    Add Core class library with the interface class.
    Add usage.
    This argument example doesn't consider to extend more calculation.

--- SampleCalc/Program.cs
+++ SampleCalc/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SampleCalc.Core;
 
 namespace SampleCalc
 {
@@ -6,7 +7,27 @@ namespace SampleCalc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            if (args.Length != 2)
+            {
+                PrintUsage();
+                return;
+            }
+
+            Console.WriteLine(CalcCore.Plus(args[0], args[1]));
+        }
+
+        static void PrintUsage()
+        {
+            Console.WriteLine("Sample Calculator v0.01");
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("Add two numbers.                               ");
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("$ dotnet run --project SampleCalc [num1] [num2]");
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("EXAMPLE                                        ");
+            Console.WriteLine("$ dotnet run --project SampleCalc 10 20        ");
+            Console.WriteLine("30                                             ");
+            Console.WriteLine("-----------------------------------------------");
         }
     }
 }
